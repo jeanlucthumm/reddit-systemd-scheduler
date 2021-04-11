a228228f842fb27f79c15e89353666cba67fc0bf
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2021-04-10 17:47:53 -0700

    Server database runs in own thread and uses message passing to talk to rpc handlers

--- server.py
+++ server.py
@@ -3,7 +3,9 @@ import configparser
 import logging
 import os
 import sqlite3
-from threading import Lock
+import threading
+from queue import Queue
+import queue
 import time
 import sys
 
@@ -14,8 +16,10 @@ from systemd import journal
 import reddit_pb2 as rpc
 import reddit_pb2_grpc as reddit_grpc
 
-# Logging setup
 LOG_LEVEL = logging.DEBUG
+LOCK_TIMEOUT = 2  # seconds
+
+# Logging setup
 log = logging.getLogger()
 log.addHandler(journal.JournalHandler())
 stdout_handler = logging.StreamHandler(sys.stdout)
@@ -51,22 +55,64 @@ def validate_post(post):
     return post.title != "" and post.subreddit != "" and post.scheduled_time != 0
 
 
+class DbCommand:
+    def __init__(self, command, obj):
+        self.command = command
+        self.obj = obj
+        self.oneshot = Queue(maxsize=1)
+
+    def reply_to_sender(self, reply):
+        self.oneshot.put_nowait(reply)
+
+    def __str__(self):
+        return f"DbCommand {self.command}: obj: {self.obj}"
+
+
 class Database:
-    def __init__(self, db_path):
+    def __init__(self, path):
+        self.path = path
+        self.queue = Queue(100)
+        # We initialize the connection in start() so that all SQL components are
+        # running in the same thread
+        self.conn = None
+
+    def queue_command(self, command):
+        log.debug("Database got command: %s", command)
         try:
-            self.conn = sqlite3.connect(db_path)
+            self.queue.put(command, timeout=LOCK_TIMEOUT)
+        except queue.Full:
+            raise Exception("Service timeout: service may be overloaded")
+
+    def start(self):
+        # Initialize
+        try:
+            self.conn = sqlite3.connect(self.path)
         except Exception as e:
-            raise Exception(f"Failed to initialize db at {db_path}") from e
+            raise Exception(f"Failed to initialize db at {self.path}") from e
         try:
             cur = self.conn.cursor()
             cur.execute(QUERY_CREATE_TABLE)
         except Exception as e:
             raise Exception("Failed to create database table") from e
-        self.lock = Lock()
+
+        # Handle commands
+        while True:
+            entry = self.queue.get()
+            log.debug("Database handling command: %s", entry)
+            command = entry.command
+            if command == 'quit':
+                log.debug('Stopping database')
+                break
+            elif command == "post":
+                try:
+                    result = self.add_post(entry.obj)
+                    entry.reply_to_sender(result)
+                except:
+                    log.exception("Failed handling %s command", command)
+                    entry.reply_to_sender("internal error. See service logs")
 
     def add_post(self, post):
         try:
-            self.lock.acquire()
             if not validate_post(post):
                 return "invalid post"
 
@@ -78,8 +124,6 @@ class Database:
             self.conn.commit()
         except sqlite3.Error as e:
             raise Exception(f"Failed to insert post into database:\n{post}") from e
-        finally:
-            self.lock.release()
 
 
 def post_to_reddit(reddit, post):
@@ -98,9 +142,16 @@ class Servicer(reddit_grpc.RedditSchedulerServicer):
     def SchedulePost(self, request, context):
         log.debug("Got SchedulePost RPC")
         try:
-            msg = self.db.add_post(request)
+            command = DbCommand("post", request)
+            self.db.queue_command(command)
+            msg = command.oneshot.get(timeout=LOCK_TIMEOUT)
             msg = msg if not None else ""
             return rpc.SchedulePostReply(error_msg=msg)
+        except queue.Empty:
+            log.exception(
+                "SchedulePost RPC timed out waiting for database with request:\n%s",
+                request,
+            )
         except Exception as e:
             log.exception("Error handling SchedulePost RPC with request:\n%s", request)
             return rpc.SchedulePostReply(error_msg="internal server error. check logs")
@@ -136,14 +187,22 @@ class Poster:
         post_to_reddit(self.reddit, TEST_POST)
 
 
+def database_thread(db):
+    log.debug('Starting database with path %s', db.path)
+    db.start()
+
+
 if __name__ == "__main__":
     db = Database(os.environ["DBPATH"])
+    threading.Thread(target=database_thread, args=(db,)).start()
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     reddit_grpc.add_RedditSchedulerServicer_to_server(
         Servicer().link_database(db), server
     )
+
     addr = "[::]:50051"
     server.add_insecure_port(addr)
-    log.info("Starting server on %s", addr)
+    log.info("Starting rpc server on %s", addr)
     server.start()
     server.wait_for_termination()
